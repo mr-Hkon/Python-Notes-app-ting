42f52164ece201d93eacc41484064aec76b7f596
Hkon <69757109+mr-Hkon@users.noreply.github.com>
2024-04-30 08:26:27 +0200

    yay its working perfect now
    🥳

--- main.py
+++ main.py
@@ -6,7 +6,21 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
-cred = credentials.Certificate("C:/Users/HåkonEllingsen/Desktop/FIREBASEPY/tryy-f9c82-firebase-adminsdk-bs40e-1da15df9d4.json")
+acountkey = {
+  "type": "service_account",
+  "project_id": "tryy-f9c82",
+  "private_key_id": "1da15df9d4c2e8e96b161dffac2a6f280e2d4a1d",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCXOZtnkz25PHhX\n7uXADpWCisIlkN+qd8zyc2bBT48zb4gtJhXYNrNC+0j6fX/IVSH3jn8xX7ea3dfD\nut6LSUCNtGcF9KGwIEiq2pSIJx8UhAbAJlPcFT19N10VouTLPD/VCa3uTZICj7il\n3X0cH65IOL2ALHcncuWv5rVbnjO3a9+CQ8dFW8T2GccELbsu3/GvbcWxlFemeIDE\nOBivUtQ+ok/BLutYRnkQ4eo3PDdCWBI5cUOONBhVXRuIG6zdV0DOoS6pj4wMInvM\neaydWfNIzySdqL81yVb26BwxnIQFbpd7zyVVWqvaSyDMjsFWF+Hbi4+mdzTTz1hj\n26Wu8E95AgMBAAECggEAAguUibg8+3yRHtcu+256nlG166JUY8ZvRWWTmm83rE+d\n8KCZ9UpmeBMEI2Fb/R6ITaVjl8Umua0Lj68sJh9TS7dUgSV3rYguZJnlotkZyaNW\nOBwjHFKIjRVWqIXwQxGyz4fZJjAiRgvkHfRh2lL7vChuEn7MMtyzQ0B7/uCtZ3SL\nY+baLI/eSmDnL5APO7vZCku8My34tGD37q9qaIC11q1u9Na7dN9Hu/FhNxuipdRL\nLsVBTM0T/7POQxcGWq6MjWXclYLtpTDqo502LLP5N+LEhIAQZrQS61Fksmslm+jj\nlbyQB+apSaikZ0IVgPt1J9yW1kk3DGaqYhtoOGTNwwKBgQDPsVNRuheko4myTDVJ\n7VW8aIrBvuJ70fk8B/QXQABDCKcrhiwC4Urhb3yRxDD7jCdQwkkgbYUj20AXPQfL\nC0SGqP6VAByUdVPWLl0/0oOPXyaiA+SwcyChGhTdvIVPx6MxjMcJekDEZT4ocDqd\nEokjzBDyxJPmNkk8PChxd1TuNwKBgQC6ZgUx9mWikeCJkZOw34T3sySje+KMYebM\nCuxaPsKtSTX9re4r/Md1/M15eEfQAEkT4+VOt/Sir3lT69fdlWp3qyZ8YgU0wNcM\n7XquH5YFbtONLtsZ5M/KDtyp9bB3LyPSoQ38DCwt5DQ/latYLJ+LmbkV/UNXQzhy\nSlg4fqlXzwKBgEnoBwcpGmq6yzAIUForO3wdE2tYWs3G7VLAXcq8OpugA6TITPcF\nZpkMDB7u2qNYv1DMoisI7fzRc2ARlqlZLBxjHJy2+nSVv6ySuRoR9kDZkizLB+EG\nRtEtL8mr4SB/oQHr35PuzgdVI6ZOMX1dcDGt10YcT6j3Hl5x5c5isXVHAoGAWUdQ\nKbQYzhsM7xdZJBiLOZEGdX967T5fK0/4EOmKmTE9uPm74Dm815h0Wj/wNYJNfkdj\np6aYd+uQM0/9HtbYHeVOE7lnQPM0ja6ZWhkq5ANHNJJCw05C8EbaDlXtZcigYdxe\nNGEoL/rjw6jOXvdQm67PZebegEdkG+ncA4M3XSUCgYBRCZdARYYbnt3WyUXFYqDA\nYKY4BaSpbXkzumOT9nmx/yGNXlHXa9UeZw5y4Bu2p5Eeow2wErxPuGT7IPsZJ5Lw\nzzCV9ytULcGWbDIiHVuUuDiOEW0FM+E26M11GVx9Kk+V95Y/59Xnkj/h212UHtmf\nXf5yWBhJe3z1gOteyzn9Lw==\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-bs40e@tryy-f9c82.iam.gserviceaccount.com",
+  "client_id": "115243368868211466673",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-bs40e%40tryy-f9c82.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
+
+cred = credentials.Certificate(acountkey)
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://tryy-f9c82-default-rtdb.europe-west1.firebasedatabase.app'
 })
@@ -14,7 +28,8 @@ firebase_admin.initialize_app(cred, {
 
 
 
-plas = db.reference('/mby/savedtext')
+plas = db.reference('/lagrafiler')
+#plas.set('')
 dataa =plas.get()
 print(dataa)
 
@@ -61,7 +76,7 @@ for i in range(100):
 for j in range(200):
     side1.grid_columnconfigure(j, weight=1)
 
-lagratekst = ""
+#lagratekst = ""
 currentfile = ""
 
 
@@ -73,17 +88,23 @@ def lagra(currentfile):
     global lagratekst
     global savedfire
     if currentfile != "":
-        fixafil = currentfile.replace('.json','')
+        fixafil = os.path.basename(currentfile).replace('.json','')
         savedfire = db.reference(f'/lagrafiler/{fixafil}')
+        print(f"the fixafil is: {fixafil}")
         lagratekst = inputtxt.get("1.0", tk.END)
+        print(f"det som lagres e denna texten: {lagratekst}")
+        try:
+            savedfire.set(lagratekst)
+        except Exception as e:
+            print(f"Error saving: {e}")
         savedplace
         savethis = fixafil + '.json'
         #file
 
         with open(currentfile, "w") as f:
             json.dump(lagratekst, f)
-        savedfire.set(lagratekst)
-        print(savedfire)
+    else:
+        print("currentfile is empty")
 
 def load(file_path):
     global lagratekst
